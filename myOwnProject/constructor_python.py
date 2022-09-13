716813bccb8539ca028059a4033b5554a0aeb343
Priyanshee S <Priyanshisingh.ap97@gmail.com>
2022-09-13 17:20:11 +0530

    constuctor

---
+++ myOwnProject/constructor_python.py
@@ -0,0 +1,15 @@
+class ckns:
+    def __init__(self):
+        print("hello priya")
+
+obj1=ckns()
+#constuctor with argument
+class cons:
+    def __init__(self,cloth,tshirt,top):
+        self.cloth=cloth
+        self.tshirt=tshirt
+        self.top=top
+
+
+shirt=cons("middy","bagn","crop_top")
+print(shirt)
