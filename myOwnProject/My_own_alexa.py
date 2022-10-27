5b63c515239e85ad6f9da0ef9e3d37bc05c1afe0
Priyanshee S <Priyanshisingh.ap97@gmail.com>
2022-10-27 14:24:31 +0530

    schedulers

---
+++ myOwnProject/My_own_alexa.py
@@ -0,0 +1,11 @@
+try:
+    a=[1,2,3,4,3,2,1]
+    for i in a:
+        if a.count(i)==1:
+            print(i)
+except Exception as df:
+    []
+
+#    a=[1,2,3,4,3,2,1]
+#c=[x for x in a if a.count(x) == 1]
+#print(c)
