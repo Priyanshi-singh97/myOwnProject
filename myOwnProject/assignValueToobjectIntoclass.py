7372d03b61fd11d40bd0bc948cb9a1d544d5eb6d
Priyanshee S <Priyanshisingh.ap97@gmail.com>
2022-11-03 20:45:22 +0530

    assing value to object while callin the constructor

---
+++ myOwnProject/assignValueToobjectIntoclass.py
@@ -0,0 +1,8 @@
+class student:
+    def __init__(self,firstname,lastname):
+        self.firstname=firstname
+        self.lastname=lastname
+        
+obj=student('p','s')
+print(obj.firstname)
+print(obj.lastname)
